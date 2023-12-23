93106f422a87a78afb18843ef74de2b2165de7c9
DevAzeredo <developer.azeredo@gmail.com>
2023-12-22 22:03:09 -0300

    setup

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: process.env.MONGODB_URI,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
